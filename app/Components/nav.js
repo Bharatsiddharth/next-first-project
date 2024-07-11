f5b170854ebd2b244e594f1a91b5a67049300a35
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-07-12 00:25:49 +0530

    form popup

--- app/Components/nav.js
+++ app/Components/nav.js
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import { gsap } from 'gsap';
 import Header from './header';
-import Link from 'next/link';
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -39,32 +38,35 @@ const Nav = () => {
 
       gsap.to('.nav2', {
         y: 0,
-        duration: 1, // Set duration to 1 second
+        duration: 1,
         ease: 'power4.inOut',
         onComplete: animateLetters,
       });
     } else {
       gsap.to('.nav2', {
         y: '-100%',
-        duration: 1, // Set duration to 1 second
+        duration: 1,
         ease: 'power4.inOut',
       });
     }
-
   }, [menuOpen]);
 
   useEffect(() => {
     if (formOpen) {
-      gsap.fromTo(".form-container", 
-        { y: "-130%", }, // Start from off the top of the screen
-        { y: "0%", duration: 2, ease: "power3.inOut" } // Move to its final position smoothly
+      gsap.fromTo(".MainForm", 
+        { scale: 0, opacity: 0 }, 
+        { scale: 1, opacity: 1, duration: 0.5, ease: "power2.out", delay: 0.2 } 
       );
     } else {
-      gsap.to(".form-container", {
-        y: "-150%",
-        
-        duration: 0.2, // Set duration to 2 seconds
-        ease: "power3.inOut",
+      gsap.to(".MainForm", {
+        y: 20,
+        scale: 0, 
+        opacity: 0, 
+        duration: 0.5, 
+        ease: "power2.in",
+        onComplete: () => {
+          gsap.set(".MainForm", { display: "none" });
+        }
       });
     }
   }, [formOpen]);
@@ -85,8 +87,8 @@ const Nav = () => {
           <div>
             <nav
               className={`nav1 z-[901] ${menuOpen
-                 ? 'bg-white transition-colors delay-200 duration-500'
-                  : 'bg-[#1D1D1F] transition-colors delay-500 duration-500'
+                ? 'bg-white transition-colors delay-200 duration-500'
+                : 'bg-[#1D1D1F] transition-colors delay-500 duration-500'
                 } max-[620px]:border-none border-transparent z-20 absolute w-full flex justify-between p-3 px-10 max-[620px]:px-4`}
             >
               <h1 className="z-[1000] font-bold max-[620px]:text-3xl text-4xl text-purple-500">
@@ -97,13 +99,12 @@ const Nav = () => {
 
                 <button
                   className={`${menuOpen
-                     ? 'opacity-0 transition delay-1000 duration-500'
-                      : 'opacity-1 transition delay-1000 duration-500'
+                    ? 'opacity-0 transition delay-1000 duration-500'
+                    : 'opacity-1 transition delay-1000 duration-500'
                     } main-btn overflow-hidden md:inline-block text-sm font-bold px-8 py-4 md:mr-5 bg-transparent text-white border-2 border-[#9d6cff] rounded-full group`}
                   onClick={toggleForm}
                 >
                   <span className="span -mt-[.2]"></span>
-                  {/* <span className='span '></span> */}
                   <span className="span top-[3.2rem]"></span>
                   <span className="span"></span>
                   <span className="transition inline-block opacity-100 group-hover:opacity-0 duration-700 ease-in-out transform group-hover:-translate-y-4">
@@ -115,9 +116,8 @@ const Nav = () => {
                   </span>
                 </button>
 
-
                 <i
-                  className={`menu rounded-full max-[620px]:ml-2  text-white max-[620px]:text-xl bg-[#9d6cff] p-4 text-2xl cursor-pointer ${menuOpen? 'ri-close-circle-line' : 'ri-menu-line'
+                  className={`menu rounded-full max-[620px]:ml-2 text-white max-[620px]:text-xl bg-[#9d6cff] p-4 text-2xl cursor-pointer ${menuOpen ? 'ri-close-circle-line' : 'ri-menu-line'
                     }`}
                   onClick={toggleMenu}
                 ></i>
@@ -125,13 +125,13 @@ const Nav = () => {
             </nav>
 
             <nav
-              className={`nav2 absolute z-[99] w-full transition-transform duration-100 ease-in-out ${menuOpen? 'transformtranslate-y-0' : 'transform -translate-y-full'
+              className={`nav2 absolute z-[99] w-full transition-transform duration-100 ease-in-out ${menuOpen ? 'transformtranslate-y-0' : 'transform -translate-y-full'
                 }`}
             >
               <div className="flex flex-col-reverse lg:flex-row items-start h-screen lg:justify-between px-10 bg-white">
                 <div className={`nav2-left mt-5 lg:mt-96 ${menuOpen
-                    ? 'opacity-1 transition delay-1000 duration-500'
-                    : 'opacity-0 transition delay-1000 duration-500'
+                 ? 'opacity-1 transition delay-1000 duration-500'
+                  : 'opacity-0 transition delay-1000 duration-500'
                   } `}>
                   <p className="text-s font-bold text-[#9d6cff] mt-0 lg:mt-20">
                     Get in Touch
@@ -144,8 +144,8 @@ const Nav = () => {
                 <div
                   id="offering"
                   className={`menu-elem ${menuOpen
-                      ? 'opacity-1 transition delay-1000 duration-500'
-                      : 'opacity-0 transition delay-1000 duration-500'
+                   ? 'opacity-1 transition delay-1000 duration-500'
+                    : 'opacity-0 transition delay-1000 duration-500'
                     } z-30 mt- lg:mt-20 mr-0 lg:mr-36 text-6xl font-semibold lg:text-8xl tracking-[10px] font-serif`}
                 >
                   <h2 className="">work</h2>
@@ -157,7 +157,7 @@ const Nav = () => {
                 </div>
               </div>
               <div
-                className={`w-full z-[1000] ${menuOpen ? ' h-[70vh]' : ' h-[0] delay-300 duration-500'
+                className={`w-full z-[1000] ${menuOpen? '-[70vh]' : '-[0] delay-300 duration-500'
                   } transition-all duration-1000 rounded-b-full bg-white`}
               ></div>
             </nav>
@@ -165,12 +165,10 @@ const Nav = () => {
         </section>
       </nav>
 
-    
-
       {formOpen && (
-        <div className='Form z-[1000]  fixed top-[50%] left-[50%]  overflow-hidden transform translate-x-[-50%] translate-y-[-50%]'>
-         <i onClick={toggleForm} class="ri-close-circle-fill text-3xl p-5 text-[#9d6cff] absolute right-0"></i>
-          <div className='w-[70vw] h-[35rem] bg-[#1d1d1fd2] flex items-center justify-center'>
+        <div className='MainForm z-[1000]  fixed top-[50%] left-[50%] overflow-hidden transform translate-x-[-50%] translate-y-[-50%]'>
+          <i onClick={toggleForm} className="close-icons ri-close-circle-fill  text-3xl p-5 text-[#9d6cff] absolute right-0"></i>
+          <div className='w-[70vw] max-[620px]:w-[28rem] h-[35rem] bg-[#1d1d1fd2] flex items-center justify-center'>
             <div className="z-[999] form-container bg-gradient-to-r from-[#212121] to-[#212121] bg-padding border-2 border-transparent p-8 text-sm font-inherit text-white flex flex-col gap-5 box-border rounded-lg bg-[length:200%_100%] animate-gradient">
               <form className="form flex flex-col gap-5">
                 <div className="form-group flex flex-col gap-1">
